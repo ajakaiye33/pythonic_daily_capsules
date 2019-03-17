02f4e5c0c7e4fbe8b108eb2f084b312540cf72d4
ajakaiye33 <ajakzhedgar@gmail.com>
2019-03-17 09:01:50 -0700

    first commit

---
+++ warm_greeting.py
@@ -0,0 +1,14 @@
+# Create a function greeting(name) that accepts a string argument (a name, ex: "Oliver")
+# , and returns a personalized greeting if a name is present
+
+
+def greetings(name):
+    """
+    greet with name
+    """
+    return 'Hello ' + name
+
+
+greet_tayo = greetings('Tayo')
+
+print(greet_tayo)
