dfeeb9999139e0416cec44c140ef89507cb4d1cb
ajakaiye33 <ajakzhedgar@gmail.com>
2019-03-21 04:57:00 +0100

    work it

---
+++ max_of_three.py
@@ -0,0 +1,12 @@
+def max_of_three(a, b, c):
+    """
+    Create the function maxOfThree that evaluates
+    three numbers and returns the largest number
+    out of the three.
+    The function takes three number arguments
+    and returns the highest number out of three.
+    """
+    return max([a, b, c])
+
+
+print(max_of_three(5, 4, 10))
