cac77cead6e5f3e0fd1db472b2d2046821c01412
ajakaiye33 <ajakzhedgar@gmail.com>
2019-03-24 05:50:25 +0100

    adjusted

--- reverse_string.py
+++ reverse_string.py
@@ -1,19 +1,20 @@
-# # def reverse_string(sen):
-# #     """
-# #     Create the function "reverseString" that
-# #     reverses a string, and returns the value
-# #     of the reversed string.
-# #     """
-# #     return sen[::-1]
-#
-#
-# print(reverse_string('hello'))
+def reverse_string(sen):
+    """
+    Create the function "reverseString" that
+    reverses a string, and returns the value
+    of the reversed string.
+    """
+    return sen[::-1]
+
+
+print(reverse_string('hello'))
+
 ################## alternatively #################
 
 
-# def reverse_string(shady):
-#     lst_str = list(shady)
-#     return ''.join(reversed(lst_str))
-#
-#
-# print(reverse_string('hello'))
+def reverse_string(shady):
+    lst_str = list(shady)
+    return ''.join(reversed(lst_str))
+
+
+print(reverse_string('hello'))
