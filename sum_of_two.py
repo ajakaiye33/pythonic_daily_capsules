b886fb1a78e86d4b0957d787e0570c05e7fbce0f
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-04 04:08:59 -0700

    sum of two

---
+++ sum_of_two.py
@@ -0,0 +1,19 @@
+def sum_of_two(num1, num2):
+    """
+    takes two numbers as argument and out their sum
+    """
+    return num1 + num2
+
+
+print(sum_of_two(2, 3))
+print(sum_of_two(7, 1))
+
+
+def return_none():
+    """
+    the function when evoked, returns "None"
+    """
+    return None
+
+
+print(return_none())
