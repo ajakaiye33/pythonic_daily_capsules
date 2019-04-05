039a4ac05fea352036f55bf13b118062867fe943
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-05 03:39:34 -0700

    string length

---
+++ string_length.py
@@ -0,0 +1,11 @@
+
+def string_length(stringy):
+    """
+    A function that receives a string and returns the length.
+    """
+    return len(stringy)
+
+
+print(string_length("abc"))
+print(string_length("Hello world"))
+print(string_length(""))
