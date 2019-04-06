727d8d1e263d151aa698287e1e8e5b244e220892
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-06 06:25:14 -0700

    reverse

---
+++ reverse_a_string.py
@@ -0,0 +1,10 @@
+
+def reverse_a_string(stringy):
+    """
+    Receives a string and returns it reversed
+    """
+    return stringy[::-1]
+
+
+print(reverse_a_string("Hello World"))
+print(reverse_a_string("Python"))
