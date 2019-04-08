e9edc3c817ff7a99fe087339ead47eb23d55c0d6
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-08 04:50:30 -0700

    boolbinary

---
+++ bool_to_binary.py
@@ -0,0 +1,11 @@
+def bool_to_binary(bol):
+    """
+    Receives a boolean and returns '1' if it's true and '0' if it's false
+    """
+    if bol == True:
+        return 1
+    return 0
+
+
+print(bool_to_binary(True))
+print(bool_to_binary(False))
