cf8eda540de43c64fdd510d402189a7513f6285f
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-08 04:51:38 -0700

    greaterthan20

---
+++ greater_20.py
@@ -0,0 +1,14 @@
+
+def greater_20(num):
+    """
+    Receives a number and if it's greater than 20 return 'True' and if not
+    returns 'False'
+    """
+    if num > 20:
+        return True
+    return False
+
+
+print(greater_20(42))
+print(greater_20(19))
+print(greater_20(42))
