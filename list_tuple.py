775e1b88910a70a5d2ef363e0b20b206f9263c96
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-17 05:42:19 -0700

    turn to tuple

---
+++ list_tuple.py
@@ -0,0 +1,10 @@
+
+def list_2_tuple(lst):
+    """
+    Receives a list and returns a tuple containing the same elements
+    """
+    return tuple(lst)
+
+
+print(list_2_tuple([1, "c", 9, False]))
+print(list_2_tuple([2]))
