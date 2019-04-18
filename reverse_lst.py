f5c74d69dc480eb1c33f7b5eda552c4b518dfcd3
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-18 02:39:55 -0700

    rev

---
+++ reverse_lst.py
@@ -0,0 +1,11 @@
+
+def reverse_lst(lst):
+    """
+    Receives a list and returns the list reversed
+    """
+    lst.reverse()
+    return lst
+    # return lst[::-1] alternatively
+
+
+print(reverse_lst([1, 2, 3, 4]))
