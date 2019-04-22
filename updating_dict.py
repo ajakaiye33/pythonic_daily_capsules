8a08b1ffc9fae565730323d20248c5cbbd3404bf
ajakaiye33 <ajakzhedgar@gmail.com>
2019-04-22 07:45:01 -0700

    update dict

---
+++ updating_dict.py
@@ -0,0 +1,12 @@
+
+rmotr_dict = {
+    "name": "RMOTR",
+    "courses": ['intro to Python', 'advanced Python'],
+}
+
+rmotr_dict["name"] = "rmotr.come"
+rmotr_dict["email"] = "questions@rmotr.com"
+rmotr_dict["courses"].append("Django Web Development")
+
+
+print(rmotr_dict)
