080ffe752693519874aa541d6ee4c91d34447c99
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-06 09:28:40 -0700

    simple class attributes

---
+++ simple_class_attr.py
@@ -0,0 +1,12 @@
+class Cookie(object):
+    """
+    class that contain two class attributes:
+    DEFAULT_SCARF_COLOR: must be "Green"
+    DEFAULT_BUTTON_COLOR: must be "Blue"
+    """
+    DEFAULT_SCARF_COLOR = "Green"
+    DEFAULT_BUTTON_COLOR = "Blue"
+
+
+print(Cookie.DEFAULT_SCARF_COLOR)
+print(Cookie.DEFAULT_BUTTON_COLOR)
