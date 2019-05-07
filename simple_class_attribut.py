bbe0d0f7f0575afbbac2697c7a1d945c819a57bd
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-07 03:50:42 -0700

    simple class

---
+++ simple_class_attribut.py
@@ -0,0 +1,11 @@
+
+class Cookie(object):
+    DEFAULT_SCARF_COLOR = "Green"
+    DEFAULT_BUTTON_COLOR = "Blue"
+
+    DEFAULT_SCARF_COLOR = "Yellow"
+    DEFAULT_BUTTON_COLOR = "Red"
+
+
+print(Cookie.DEFAULT_SCARF_COLOR)
+print(Cookie.DEFAULT_BUTTON_COLOR)
