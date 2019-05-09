1e38cc239ec2e81d58ef1677b9f01fd77bc34e5b
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-09 04:57:00 -0700

    cookie

---
+++ cookie_factory.py
@@ -0,0 +1,13 @@
+
+class Cookie(object):
+
+    self.cookies = 'cookies'
+
+    def creat_cookies(num):
+        combine = []
+        for i in range(1, num):
+            print(combine.append(Cookie.cookies))
+
+
+c = Cookie()
+c.creat_cookies(2)
