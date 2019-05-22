63a0891a3466d549ab65e75b2fc993a310a28318
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-22 06:19:20 -0700

    runny

--- apythonicworkout_book/run_time.py
+++ apythonicworkout_book/run_time.py
@@ -9,16 +9,31 @@ def runny_time():
     bag_time = 0
     toge_time = 0
     while True:
-        today_time = int(input("Enter 10km run time > "))
+        today_time = input("Enter 10km run time > ")
 
         if today_time == "q":
             break
         else:
             toge_time += 1
-            bag_time += today_time
-            average_time = bag_time/toge_time
+            bag_time += int(today_time)
+    average_time = bag_time/toge_time
 
-    return f" Average of {average_time} over toge_time"
+    return f" Average of {average_time} over {toge_time}"
+
+# without function
 
 
 print(runny_time())
+
+total_time = 0
+no_of_runs = 0
+while True:
+    one_time = input("Enter 10km run time > ")
+
+    if one_time == 'q':
+        break
+    else:
+        no_of_runs += 1
+        total_time += float(no_of_runs)
+average_time = total_time/no_of_runs
+print(f"average of {average_time} for {no_of_runs}")
