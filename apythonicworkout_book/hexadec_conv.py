0b33b72e2d9adf6cae303d6068a675c32ff1da9c
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-23 05:40:40 -0700

    hedxanumber

---
+++ apythonicworkout_book/hexadec_conv.py
@@ -0,0 +1,14 @@
+
+
+def hex_converter(hexnum):
+    """
+    receives a hex number and returns the decimal equivalent
+    """
+    return hex(hexnum)
+
+    # else:
+    #     return int(hexnum,16)
+    #
+
+
+print(hex_converter(20))
