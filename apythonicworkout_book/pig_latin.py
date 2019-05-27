8a8beeb98da7ebf43bdc4b3af8ac7fbc9a7327b2
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-27 12:45:13 -0700

    morsel

--- apythonicworkout_book/pig_latin.py
+++ apythonicworkout_book/pig_latin.py
@@ -1,49 +1,49 @@
-#
-# def pig_latin():
-#     """
-#     The program should asks the user to enter an english word. your program should
-#     then print the word, translated into Pig Latin.
-#     If the word begins with a vowel(a,e,i,o,u), then add way to the end of the word e.g
-#     so "air" becomes "airway" eat becomes "eatway"
-#     if the word begin with any other letter, put it on the end of the word and then add "ay"
-#     so python  becomes "ythonpay"
-#     """
-#
-#     your_word = input("type a word > ")
-#     vowel = "aeiou"
-#     if your_word[0] in vowel:
-#         wayword = your_word[1:] + "way"
-#     else:
-#         frst_word = your_word[0]
-#         wayword = your_word[1:] + frst_word + "ay"
-#     return wayword
-#
-#
-# print(pig_latin())
 
-# Bonus
+def pig_latin():
+    """
+    The program should asks the user to enter an english word. your program should
+    then print the word, translated into Pig Latin.
+    If the word begins with a vowel(a,e,i,o,u), then add way to the end of the word e.g
+    so "air" becomes "airway" eat becomes "eatway"
+    if the word begin with any other letter, put it on the end of the word and then add "ay"
+    so python  becomes "ythonpay"
+    """
+
+    your_word = input("type a word > ")
+    vowel = "aeiou"
+    if your_word[0] in vowel:
+        wayword = your_word[1:] + "way"
+    else:
+        frst_word = your_word[0]
+        wayword = your_word[1:] + frst_word + "ay"
+    return wayword
 
 
-# def pig_latin2():
-#     """
-#     if the word contains two different vowels "way" should added at its end but if
-#     it contain one vowel the first word should placed at it end and subsequently add "ay"
-#     """
-#     the_vow = "aeiou"
-#     counter = []
-#     ask_word = input("Give us a word > ")
-#     for i in the_vow:
-#         if i in ask_word:
-#             counter.append(i)
-#             if len(counter) == 2:
-#                 res = f"{ask_word}way"
-#             else:
-#                 res = f"{ask_word[1:]}{ask_word[0]}ay"
-#
-#     return res
-#
-#
-# print(pig_latin2())
+print(pig_latin())
+
+Bonus
+
+
+def pig_latin2():
+    """
+    if the word contains two different vowels "way" should added at its end but if
+    it contain one vowel the first word should placed at it end and subsequently add "ay"
+    """
+    the_vow = "aeiou"
+    counter = []
+    ask_word = input("Give us a word > ")
+    for i in the_vow:
+        if i in ask_word:
+            counter.append(i)
+            if len(counter) == 2:
+                res = f"{ask_word}way"
+            else:
+                res = f"{ask_word[1:]}{ask_word[0]}ay"
+
+    return res
+
+
+print(pig_latin2())
 
 
 def pig_latin3():
@@ -59,3 +59,17 @@ def pig_latin3():
 
 
 print(pig_latin3())
+
+
+# Morsels python problem
+
+def unique_only(lst):
+    return [i for i in set(lst)]
+
+
+print(unique_only([1, 2, 2, 1, 1, 3, 2, 1]))
+# print(unique_only())
+
+nums = [1, -3, 2, 3, -1]
+sqr = [i**2for i in nums]
+print(unique_only(sqr))
