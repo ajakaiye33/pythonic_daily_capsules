402d531e4bddeb13140adcccbe6f1a5846c1dfbb
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-29 06:38:39 -0700

    ubbi dubbi

--- apythonicworkout_book/lang_interpret.py
+++ apythonicworkout_book/lang_interpret.py
@@ -1,17 +1,32 @@
 
-def ubbi_dubbi():
-    """
-    receives a word to the "ubbi dubbi" language. That is every vowel letter in the
-    word should be prefaced with "ub"
-    """
-    get_words = input("Let have your word > ")
-    vow_letter = "aeiou"
-    cob = []
-    for letr in get_words:
-        for apha in vow_letter:
-            if letr == apha:
-                seeyah = get_words.replace(letr, "ub")
-    return seeyah
+# def ubbi_dubbi():
+#     """
+#     receives a word and translate to the "ubbi dubbi" language. That is every vowel letter in the
+#     word should be prefaced with "ub"
+#     """
+#     get_words = input("Let have your word > ")
+#     vow_letter = "aeiou"
+#     cob = []
+#     for letr in get_words:
+#         for apha in vow_letter:
+#             if letr == apha:
+#                 seeyah = get_words.replace(letr, "ub")
+#     return seeyah
+#
+#
+# print(ubbi_dubbi())
 
 
-print(ubbi_dubbi())
+def deleh():
+    the_choice = "elephant"
+    the_serc = "aeiou"
+    comb = []
+    for i in the_choice:
+        if i in the_serc:
+            comb.append(f"ub{i}")
+        else:
+            comb.append(i)
+    return "".join(comb)
+
+
+print(deleh())
