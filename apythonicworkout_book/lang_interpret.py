4ecab5d3ca18b79e646bf9fd9a1050cf59978ecb
ajakaiye33 <ajakzhedgar@gmail.com>
2019-05-30 04:15:39 -0700

    add bonus

--- apythonicworkout_book/lang_interpret.py
+++ apythonicworkout_book/lang_interpret.py
@@ -1,32 +1,53 @@
 
-# def ubbi_dubbi():
-#     """
-#     receives a word and translate to the "ubbi dubbi" language. That is every vowel letter in the
-#     word should be prefaced with "ub"
-#     """
-#     get_words = input("Let have your word > ")
-#     vow_letter = "aeiou"
-#     cob = []
-#     for letr in get_words:
-#         for apha in vow_letter:
-#             if letr == apha:
-#                 seeyah = get_words.replace(letr, "ub")
-#     return seeyah
-#
-#
-# print(ubbi_dubbi())
-
-
-def deleh():
-    the_choice = "elephant"
-    the_serc = "aeiou"
-    comb = []
-    for i in the_choice:
-        if i in the_serc:
-            comb.append(f"ub{i}")
+def ubbi_dubbi():
+    """
+    receives a word and translate to the "ubbi dubbi" language. That is every vowel letter in the
+    word should be prefaced with "ub"
+    """
+    result = []
+    the_vowel = "aeiou"
+    get_words = input("Let have your world > ")
+    for i in get_words:
+        if i in the_vowel:
+            result.append(f"ub{i}")
         else:
-            comb.append(i)
-    return "".join(comb)
+            result.append(i)
+    return "".join(result)
 
 
-print(deleh())
+print(ubbi_dubbi())
+
+# Bonus
+
+
+def cap_ubbi():
+    """
+    if the word is capitalize, ubbi_dubbi translation should be capitalize
+    """
+    see_aske = input("The word")
+    see_aske = see_aske.lower()
+    gather = []
+    vywel = "aioue"
+
+    for k in see_aske:
+        if k in vywel:
+            gather.append(f"ub{k}")
+        else:
+            gather.append(k)
+
+    return "".join(gather).title()
+
+
+print(cap_ubbi())
+
+Bonus
+
+
+def remove_author(lst_name):
+    temp_house = []
+    for i in lst_name:
+        temp_house.append(i.replace(i, "_"))
+    return temp_house
+
+
+print(remove_author(["Aloba", "Francis", "Nnamdi", "Osita", "Garry"]))
