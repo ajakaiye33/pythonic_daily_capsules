34297c29bf0f9f97fc502cda97f62d526710dc28
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-01 05:59:06 -0700

    sortedwith commer

--- apythonicworkout_book/sort_word.py
+++ apythonicworkout_book/sort_word.py
@@ -11,3 +11,20 @@ def strsort(stringy):
 
 
 print(strsort("cba"))
+
+# Bonus
+
+
+def strsort2(stringy2):
+    """
+    Receives a string, break it into individual words and then sort those words
+    alphabetically. Then print them with commas(,) between the names
+    """
+
+    turn_sent_to_list = stringy2.split()
+    alpha_sorted = sorted(turn_sent_to_list)
+    display_with_comma = ",".join(alpha_sorted)
+    return display_with_comma
+
+
+print(strsort2("Tom Dick Harry"))
