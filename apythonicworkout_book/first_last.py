1b88f5b71d9907f8dc498dbb626049dea5c17433
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-03 07:53:17 -0700

    bonus

--- apythonicworkout_book/first_last.py
+++ apythonicworkout_book/first_last.py
@@ -1,18 +1,31 @@
 
-def firstlast(seq):
-    """
-    receives a sequence example string, list tuple and returns the first and last
-    elements of that sequence in a two element of the same type
-    """
+# def firstlast(seq):
+#     """
+#     receives a sequence example string, list tuple and returns the first and last
+#     elements of that sequence in a two element of the same type
+#     """
+#
+#     return seq[:1] + seq[-1:]
+#
+#
+# print(firstlast("abc"))
+# print(firstlast([1, 2, 3, 4, 5]))
+#
+#
+# # Bonus
+#
+#
+# def value_sqrt(integerz):
+#     return integerz**2
+#
+#
+# print(value_sqrt(1.34))
+# print(value_sqrt(3))
 
-    result = []
 
-    first_element = seq[0]
-    laste_element = seq[-1]
-    result.append(first_element)
-    result.append(laste_element)
-    return result
+def larg(v):
+    return max(v)
 
 
-print(firstlast("abc"))
-print(firstlast([1, 2, 3, 4, 5]))
+print(larg([1, 1, 2, 4]))
+print(larg("the rate of killing is alarming"))
