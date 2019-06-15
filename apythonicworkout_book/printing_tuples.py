92d7c4a9a7fb63a3f9e1ea57e717221254b5fe24
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-15 08:27:02 -0700

    adjust

--- apythonicworkout_book/printing_tuples.py
+++ apythonicworkout_book/printing_tuples.py
@@ -1,3 +1,5 @@
+import operator
+
 
 def print_tuples(lst_tup):
     for i in lst_tup:
@@ -7,3 +9,8 @@ def print_tuples(lst_tup):
 print(print_tuples([('Donald', 'Trump', 7.85),
                     ('Vladimir', 'Putin', 3.626),
                     ('Jinpinq', 'Xi', 10.603)]))
+
+# alternate solution
+for person in sorted(people, key=operator.itemgetter(1, 0)):
+    print("{1: 10} {0: 10}
+          {2: 5.2f}").format(*person)
