a968e718dacb333bac38cc9e60e6a682367a37ad
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-18 05:36:40 -0700

    double coke

---
+++ apythonicworkout_book/double_coke.py
@@ -0,0 +1,14 @@
+
+def who_is_next(names, r):
+    """
+
+    """
+    for ind, val in enumerate(names, 1):
+        if ind == r:
+            names.append(val)
+            names.append(val)
+            print(names)
+            return val
+
+
+print(who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 2))
