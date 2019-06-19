19a51639eaf36f15b311746a9e3c7795ca9c130f
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-19 06:01:21 -0700

    triple double

---
+++ apythonicworkout_book/creation_one.py
@@ -0,0 +1,11 @@
+# to be continued ...
+def triple_double(num1, num2):
+    """which takes numbers num1 and num2 and returns 1 if there is a
+    straight triple of a number at any place in num1 and also a straight
+    double of the same number in num2.If this isn't the case, return 0"""
+    vt = 0
+    frst = list(str(num1))
+    scd = list(str(num2))
+
+
+print(triple_double(451999277, 41177722899))
--- apythonicworkout_book/double_coke.py
+++ apythonicworkout_book/double_coke.py
@@ -4,11 +4,14 @@ def who_is_next(names, r):
 
     """
     for ind, val in enumerate(names, 1):
+
         if ind == r:
             names.append(val)
             names.append(val)
+            names += names
+
             print(names)
-            return val
+    return val
 
 
-print(who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 2))
+print(who_is_next(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 9))
