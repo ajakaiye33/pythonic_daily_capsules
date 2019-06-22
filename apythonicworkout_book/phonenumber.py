3fa2a9cf8fe43a9bcbb1f0c615e287f5faee0a2c
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-22 14:25:37 -0700

    added pit it

--- apythonicworkout_book/phonenumber.py
+++ apythonicworkout_book/phonenumber.py
@@ -1,12 +1,40 @@
+#
+# def create_phone_number(n):
+#     """
+#     trandsfor to phone number format
+#     """
+#     nf = ''
+#     for i in n:
+#         nf += str(i)
+#     return f"({nf[0:3]}) {nf[3:6]}-{nf[6:]}"
+#
+#
+# print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+#
 
-def create_phone_number(n):
-    """
-    trandsfor to phone number format
-    """
-    nf = ''
-    for i in n:
-        nf += str(i)
-    return f"({nf[0:3]}) {nf[3:6]}-{nf[6:]}"
+
+####
+
+# def scramble(s1, s2):
+#
+#
+# # scramble('rkqodlw', 'world') ==> True
+# # scramble('cedewaraaossoqqyt', 'codewars') ==> True
+# # scramble('katas', 'steak') ==> False
+#
+# print(scramble('rkqodlw', 'world'))
+# print(scramble('cedewaraaossoqqyt', 'codewars'))
+# print(scramble('katas', 'steak'))
+
+def pig_it(stringy):
+    """Move the first letter of each word to the end of it, then add "ay" to the end
+    of the word. Leave punctuation marks untouched.Examples
+    pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+    pig_it('Hello world !')     # elloHay orldway !"""
+    dey = ''
+    ass = stringy.split()
+    for i, j in enumerate(ass):
+        print(j[1:] + j[0] + "ay")
 
 
-print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(pig_it('Pig latin is cool'))
