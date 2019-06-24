034bbcdb02c307a7aa6c7b3abd1b3b5ae8d36e00
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-24 15:15:31 -0700

    anagram

--- apythonicworkout_book/calc.py
+++ apythonicworkout_book/calc.py
@@ -1,14 +1,36 @@
 
-def calc(expr):
-    d = expr.split()
-    #f = int(d[0])
-    #s = int(d[1])
-    #sym = d[2]
-    #ge = f"{f}{sym}{s}"
-    print(d)
+# def calc(expr):
+#     d = expr.split()
+#     #f = int(d[0])
+#     #s = int(d[1])
+#     #sym = d[2]
+#     #ge = f"{f}{sym}{s}"
+#     print(d)
+#
+#     #e = int(expr[0])
+#     # symbol = int(expr[])
+#
+#
+# print(calc("3 3 *"))
 
-    #e = int(expr[0])
-    # symbol = int(expr[])
+
+def anagrams(word, words):
+    # your code here
+    sey = []
+    for i in word:
+        for s in words:
+            if i in s:
+                sey.append(s)
+                return sey
 
 
-print(calc("3 3 *"))
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))  # ['aabb', 'bbaa']
+
+print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))  # ['carer', 'racer']
+
+# print(anagrams('laser', ['lazing', 'lazy',  'lacer']))  # []
+
+
+v = "abba"
+d = "bbaa"
+v[::-1]
