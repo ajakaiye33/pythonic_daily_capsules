62839a7c7af6b1f9272ad3b93510a3d1118f3c8d
ajakaiye33 <ajakzhedgar@gmail.com>
2019-06-27 13:29:16 -0700

    double encode

--- apythonicworkout_book/perimeter_square.py
+++ apythonicworkout_book/perimeter_square.py
@@ -1,10 +1,37 @@
 
-def perimeter(n):
-    # your code
-    se = 0
-    for i in range(n+1):
-        se += i
-    return se * 4
+# def perimeter(n):
+#     # your code
+#     se = 0
+#     for i in range(n+1):
+#         se += i
+#     return se * 4
+#
+#
+# print(perimeter(5))
 
 
-print(perimeter(5))
+# The goal of this exercise is to convert a string to a new string where each
+# character in the new string is "(" if that character appears only once in
+# the original string, or ")" if that character appears more than once in the
+# original string. Ignore capitalization when determining if a character is
+# a duplicate.
+
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+
+
+def duplicate_encode(stringy):
+    dey = ''
+    lty = list(stringy)
+    for ind, vay in enumerate(lty):
+        if lty.count(vay) > 2:
+            print(")")
+        else:
+            print("(")
+
+
+print(duplicate_encode("din"))
+print(duplicate_encode("recede"))
