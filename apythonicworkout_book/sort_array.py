4eaf92dca6bc01924ee6c342c4338d8d27804046
ajakaiye33 <ajakzhedgar@gmail.com>
2019-07-02 21:22:49 -0700

    learning

--- apythonicworkout_book/sort_array.py
+++ apythonicworkout_book/sort_array.py
@@ -1,12 +1,25 @@
 
 
-def sort_array(lst):
-    foo = []
-    for i, j in enumerate(lst):
-        if j % 2 == 0:
-            print(i)
+# def sort_array(lst):
+#     foo = []
+#     boo = []
+#     for i, j in enumerate(lst):
+#         if j % 2 == 0:
+#             foo.insert(i, j)
+#         elif j % 2 != 0:
+#             boo.append(j)
+#             sorted(boo)
+#             boo[::-1]
+#     return boo + foo
 
-    # print(i.sorted())
+# print(i.sorted())
+# correct solution
+def sort_array(lst):
+    result = [i for i in lst if i % 2 != 0]
+    for indy, val in enumerate(lst):
+        if val % 2 == 0:
+            result.insert(indy, val)
+    return result
 
 
 print(sort_array([5, 3, 2, 8, 1, 4]))  # == [1, 3, 2, 8, 5, 4]
