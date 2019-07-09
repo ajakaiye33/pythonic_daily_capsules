50202398eaad09340ee5354d6c6704b4cedd45f2
ajakaiye33 <ajakzhedgar@gmail.com>
2019-07-08 22:26:34 -0700

    generate hashtag

---
+++ apythonicworkout_book/hastag_gen.py
@@ -0,0 +1,20 @@
+
+def generate_hashtag(stringy):
+    """
+    receives a sentence return same with hashtag added if it is not empty string
+    and the len is less than 140
+    """
+    uper = stringy.title()
+    stripy = uper.split()
+    stripy.insert(0, "#")
+    comby = "".join(stripy)
+    if len(comby) > 140 or stringy == "":
+        return False
+    else:
+        return comby
+
+
+print(generate_hashtag("Hello there thanks for trying my Kata"))
+
+print(generate_hashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"))
+print(generate_hashtag(""))
