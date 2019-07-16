d8c644244a6c62bfcaf65c8cc870e942f43c1f10
ajakaiye33 <ajakzhedgar@gmail.com>
2019-07-15 21:12:47 -0700

    wip

--- apythonicworkout_book/valid_parenth.py
+++ apythonicworkout_book/valid_parenth.py
@@ -1,15 +1,13 @@
-# "()"              =>  true
-# ")(()))"          =>  false
-# "("               =>  false
-# "(())((()())())"  =>
+def first_non_repeating_letter(stringy):
+    """
+    receives a string and return the first no-repeating letter
+    """
+    stringy_lower = stringy.lower()
+
+    for i, j in enumerate(stringy_lower):
+        if stringy_lower.count(j) == 1:
+            return stringy_lower[i]
+    return "''"
 
 
-doo = "hi())("
-open = '('
-close = ')'
-twin = open + close
-frst = doo.count('(')
-scd = doo.count(')')
-numtwin = doo.count(twin)
-print(frst) + print(scd)
-print(numtwin)
+print(first_non_repeating_letter('stress'))
