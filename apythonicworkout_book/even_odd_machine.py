6fc0c51dbf41451b81a5c1696f8887f764e58570
ajakaiye33 <ajakzhedgar@gmail.com>
2019-07-25 23:38:31 -0700

    wip

--- apythonicworkout_book/even_odd_machine.py
+++ apythonicworkout_book/even_odd_machine.py
@@ -1,11 +1,21 @@
-def even_odd():
+def even_odd(c):
     """
     Print whether the number is even or odd.
     Display the number followed by the next 9 even or odd numbers
     """
-    for i in range(1, 100):
+
+    for i in range(1, c+1):
         if i % 2 == 0:
-            print()
+            print('even')
+            print(i)
+        elif i % 3 == 0:
+            print('odd')
+            print(i)
 
 
-even_odd()
+if __name__ == "__main__":
+    c = float(input("Enter your number: > "))
+    if c > 0 and c.is_integer():
+        even_odd(int(c))
+    else:
+        print("Please enter a valid number")
